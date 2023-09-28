01b8da49c1a2ec716edb31aa3132b80f56e3eb37
Danglr <109924850+Danglr@users.noreply.github.com>
2023-09-28 03:51:58 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,150 @@
+CONFIG = {}
+
+CONFIG.DealerPos = vector4(-1811.048, -556.9874, 155.98309, 253.00506)
+
+CONFIG.wagonid = {
+    {
+        hash = -824257932, -- The hash id of the model
+        model = 'cart01', -- The model itself
+        name = 'Cart 1', -- Whichever name you would like it to be called
+        price = 8, -- The price of the wagon
+        wagonSpawn = vector4(-1802.889, -603.457, 154.11616, 313.62756), -- Ignore this, just add 0 to it or smt
+        storage = 8, -- How many slots the wagon has
+        weight = 200000 -- How much weight there can be in the wagon
+    },
+    {
+        hash = -2053881888, 
+        model = 'cart02', 
+        name = 'Cart 2', 
+        price = 75,
+        wagonSpawn = vector4(-1798.418, -605.6962, 153.54212, 312.56396),
+        storage = 6,
+        weight = 200000
+    },
+    {
+        hash = -1347283941, 
+        model = 'cart03', 
+        name = 'Cart 3', 
+        price = 150,
+        wagonSpawn = vector4(-1794.986, -608.3958, 152.83732, 313.71353),
+        storage = 10,
+        weight = 250000
+    },
+    {
+        hash = -570691410, 
+        model = 'cart04', 
+        name = 'Cart 4', 
+        price = 225,
+        wagonSpawn = vector4(-1791.018, -611.6496, 151.76203, 320.38354),
+        storage = 10,
+        weight = 275000
+    },
+    {
+        hash = 374792535, 
+        model = 'cart05', 
+        name = 'Cornwall Oil Wagon', 
+        price = 150,
+        wagonSpawn = vector4(-1782.282, -602.8549, 151.64329, 141.97291),
+        storage = 3,
+        weight = 50000
+    },
+    {
+        hash = 219205323, 
+        model = 'cart06', 
+        name = 'Large Back-Open Cart', 
+        price = 300,
+        wagonSpawn = vector4(-1788.279, -601.3692, 153.14079, 147.14958),
+        storage = 15,
+        weight = 350000
+    },
+    {
+        hash = 47200842, 
+        model = 'cart07', 
+        name = 'Small Enclosed Cart', 
+        price = 175,
+        wagonSpawn = vector4(-1793.684, -597.9241, 154.24227, 147.7151),
+        storage = 6,
+        weight = 175000
+    },
+    {
+        hash = -377157708, 
+        model = 'cart08', 
+        name = 'Small Cloth Covered Cart', 
+        price = 185,
+        wagonSpawn = vector4(-1800.343, -593.3711, 155.12245, 146.94023),
+        storage = 6,
+        weight = 175000
+    },
+    {
+        hash = 1761016051, 
+        model = 'COACH2', 
+        name = 'Stage Coach With Doors', 
+        price = 400,
+        wagonSpawn = vector4(-1800.343, -593.3711, 155.12245, 146.94023),
+        storage = 3,
+        weight = 100000
+    },
+    {
+        hash = -136833353, 
+        model = 'COACH3', 
+        name = 'Medium Coach Doors & Windows', 
+        price = 425,
+        wagonSpawn = vector4(-1800.343, -593.3711, 155.12245, 146.94023),
+        storage = 3,
+        weight = 75000
+    },
+    {
+        hash = 93893176, 
+        model = 'COACH4', 
+        name = 'Small Coach Doors & Windows', 
+        price = 360,
+        wagonSpawn = vector4(-1800.343, -593.3711, 155.12245, 146.94023),
+        storage = 3,
+        weight = 50000
+    },
+    {
+        hash = -1826304690, 
+        model = 'COACH5', 
+        name = '2 Person Small Square Coach', 
+        price = 300,
+        wagonSpawn = vector4(-1800.343, -593.3711, 155.12245, 146.94023),
+        storage = 3,
+        weight = 50000
+    },
+    {
+        hash = -1544786211, 
+        model = 'COACH6', 
+        name = '2 Person Medium Roofless Coach', 
+        price = 285,
+        wagonSpawn = vector4(-1800.343, -593.3711, 155.12245, 146.94023),
+        storage = 3,
+        weight = 70000
+    },
+    {
+        hash = -1278978750, 
+        model = 'BUGGY01', 
+        name = 'Fancy Small 2 Person Buggy', 
+        price = 400,
+        wagonSpawn = vector4(-1800.343, -593.3711, 155.12245, 146.94023),
+        storage = 3,
+        weight = 30000
+    },
+    {
+        hash = -1100387700, 
+        model = 'BUGGY02', 
+        name = 'Small Buggy With Canopy', 
+        price = 250,
+        wagonSpawn = vector4(-1800.343, -593.3711, 155.12245, 146.94023),
+        storage = 3,
+        weight = 30000
+    },
+    {
+        hash = -1861840953, 
+        model = 'BUGGY03', 
+        name = 'Small Buggy With Medium Canopy', 
+        price = 265,
+        wagonSpawn = vector4(-1800.343, -593.3711, 155.12245, 146.94023),
+        storage = 3,
+        weight = 30000
+    },
+}
